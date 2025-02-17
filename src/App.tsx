73d910f0a11ac8afb8d0c8dca288e9688b3cc5b0
magnoliagg <mbihcijacu4619@hotmail.com>
2025-02-17 19:47:23 +0800

    init project structure
    basic react app setup with typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MoneyMind</h1>
+        <p>Personal Finance Tracker</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
